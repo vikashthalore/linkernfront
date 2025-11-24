03096a93777728e846da31daabf6479f2e71b0b4
vikashthalore <vikashthalor74@gmail.com>
2025-11-24 05:55:20 -0800

    fix: vercel rewrite + vite base

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/',
+  base: "/",  // IMPORTANT for Vercel SPA refresh
   plugins: [react(), tailwindcss()],
   server: {
     port: 5173,
