a138f5ee30cefc724aaaad878658a6cb7bc29707
vikashthalore <vikashthalor74@gmail.com>
2025-11-26 20:33:16 -0800

    finallast vercel.json again old onrender use e wala aply

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'https://linkern-kappa.vercel.app',
+        target: 'https://linkern.onrender.com',
         changeOrigin: true,
         secure: false
       }
